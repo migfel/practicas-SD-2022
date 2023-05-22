1825933e91db941fed6600ed94095feac1dec564
Miguel Felix <migfel@users.noreply.github.com>
2023-05-22 15:40:02 -0600

    Update microservicio OpenWeather.py

--- MicroServicio VS WebService OpenWeather/microservicio OpenWeather.py
+++ MicroServicio VS WebService OpenWeather/microservicio OpenWeather.py
@@ -1,18 +1,18 @@
 import requests
 city = 'Mexico'  # Inserta el nombre de la ciudad de la cual se quiere saber la temperatura 
 
-def get_temperature():
-    api_key = ''  # Inserta  tu API KEY  OpenWeatherMap 
+def get_temperatura():
+    api_key = '152b1599f3e42d9d0f559bf3cf348a2b'  # Inserta  tu API KEY  OpenWeatherMap 
     
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     
     response = requests.get(url)
     data = response.json()
     
-    temperature = data['main']['temp']
-    return temperature
+    temperatura = data['main']['temp']
+    return temperatura
 
 if __name__ == '__main__': #Para que la aplicacion se ejecute cuando es llamada directamente, 
     #si este programa es llamado como modulo en otro programa entonces la variable __name__ no tendra el valor de main y no se ejecutara 
-    temperature = get_temperature()
-    print(f'La temperatura en la  ciudad {city} es: {temperature}°C')
+    temperatura = get_temperatura()
+    print(f'La temperatura en la  ciudad {city} es: {temperatura}°C')
