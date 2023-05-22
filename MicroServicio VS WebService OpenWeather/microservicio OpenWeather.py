fc0936d2b1c2f57f587ecf87708cec994fd1783b
Miguel Felix <migfel@users.noreply.github.com>
2023-05-22 14:27:58 -0600

    Update microservicio OpenWeather.py

--- MicroServicio VS WebService OpenWeather/microservicio OpenWeather.py
+++ MicroServicio VS WebService OpenWeather/microservicio OpenWeather.py
@@ -12,6 +12,7 @@ def get_temperature():
     temperature = data['main']['temp']
     return temperature
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Para que la aplicacion se ejecute cuando es llamada directamente, 
+    #si este programa es llamado como modulo en otro programa entonces la variable __name__ no tendra el valor de main y no se ejecutara 
     temperature = get_temperature()
     print(f'La temperatura en la  ciudad {city} es: {temperature}°C')
