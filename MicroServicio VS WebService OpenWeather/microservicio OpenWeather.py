2936ff866db3997671154316a20b81b4fe80553c
Miguel Felix <migfel@users.noreply.github.com>
2023-05-22 14:18:41 -0600

    Update microservicio OpenWeather.py

--- MicroServicio VS WebService OpenWeather/microservicio OpenWeather.py
+++ MicroServicio VS WebService OpenWeather/microservicio OpenWeather.py
@@ -2,7 +2,7 @@ import requests
 city = 'Mexico'  # Inserta el nombre de la ciudad de la cual se quiere saber la temperatura 
 
 def get_temperature():
-    api_key = '152b1599f3e42d9d0f559bf3cf348a2b'  # Inserta  tu API KEY  OpenWeatherMap 
+    api_key = ''  # Inserta  tu API KEY  OpenWeatherMap 
     
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     
