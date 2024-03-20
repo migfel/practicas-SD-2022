c75a052af8ec48aa3854c9a3b11aff927c09bf5f
Miguel Felix <migfel@users.noreply.github.com>
2024-03-20 13:26:05 -0600

    Add files via upload

---
+++ SpotifyGui.py
@@ -0,0 +1,46 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import tkinter as tk
+from tkinter import messagebox
+
+# Configura las credenciales de autenticación
+client_id = '0f2dc6e535074e039f2462873de47ebd'
+client_secret = '90b6a7e8cbb64f2491211fa66a29c778'
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def search_song(query):
+    # Busca una canción
+    result = sp.search(q=query, limit=1)
+
+    # Muestra el nombre de la primera canción encontrada
+    if result['tracks']['items']:
+        track_name = result['tracks']['items'][0]['name']
+        return track_name
+    else:
+        return "No se encontraron canciones con ese nombre."
+
+def search_and_display():
+    query = entry.get()
+    track_name = search_song(query)
+    messagebox.showinfo("Canción encontrada", f"La primera canción encontrada es: {track_name}")
+
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Buscar canción")
+
+# Crear y posicionar la etiqueta
+label = tk.Label(root, text="Introduce el nombre de la canción:")
+label.pack()
+
+# Crear y posicionar la entrada de texto
+entry = tk.Entry(root)
+entry.pack()
+
+# Crear y posicionar el botón de búsqueda
+search_button = tk.Button(root, text="Buscar", command=search_and_display)
+search_button.pack()
+
+# Ejecutar el bucle de eventos de la ventana
+root.mainloop()
